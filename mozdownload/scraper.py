ca71f6f4d80bcdb09350f8cd4d02f3ae8b81e2a9
Majdi Haouech <m.haouech@criteo.com>
2018-03-19 15:34:38 +0100

    Calculate hash after the file is downloaded not while downloading

--- mozdownload/scraper.py
+++ mozdownload/scraper.py
@@ -380,7 +380,6 @@ class Scraper(object):
             for chunk in r.iter_content(CHUNK_SIZE):
                 stream.write(chunk)
                 bytes_downloaded += CHUNK_SIZE
-                hash.update(str(chunk))
 
                 if log_level <= logging.INFO and content_length:
                     pbar.update(bytes_downloaded)
@@ -389,6 +388,9 @@ class Scraper(object):
                 if self.timeout_download and \
                         t1 >= self.timeout_download:
                     raise errors.TimeoutError
+                
+            stream.seek(0)
+            hash.update(str(stream.read()))
 
             self.hashdata = hash.hexdigest()
 
